fe412b2d64318288433f849e6ad3d4be16fb20d8
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2016-04-16 20:33:34 +0530

    Drinks.cpp :metal:

---
+++ c++/Drinks.cpp
@@ -0,0 +1,40 @@
+/*
+	Author : Amar Prakash Pandey
+	contact : http://amarpandey.me
+*/
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <complex>
+#include <math.h>
+#include <set>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stdio.h>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <ctime>
+#include <memory.h>
+#include <ctime>
+#include <assert.h>
+#define pi 3.14159
+#define mod 1000000007
+#define rep(i,a,n) for (int i=a;i<=n;i++)
+#define per(i,n,a) for (int i=n;i>=a;i--)
+using namespace std;
+long long int a[500000];
+int main()
+{
+	long int n,x;
+  cin >> n;
+  double ratio=0;
+  rep(i,1,n) {
+    cin >> x;
+    ratio +=x;
+  }
+  printf("%0.12f\n",ratio/n);
+	return 0;
+}
