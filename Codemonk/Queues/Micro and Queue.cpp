16586a5318a029bce481a311533a2cd790f2b507
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-01-24 19:17:34 +0530

    codemonk stack ques

---
+++ Codemonk/Queues/Micro and Queue.cpp
@@ -0,0 +1,40 @@
+/*
+	Author : Amar Prakash Pandey
+	contact : http://amarpandey.me
+*/
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <complex>
+#include <math.h>
+#include <set>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stdio.h>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <ctime>
+#include <memory.h>
+#include <ctime>
+#include <assert.h>
+#define pi 3.14159
+#define mod 1000000007
+using namespace std;
+
+#ifndef LL
+	#define LL long long
+#endif
+
+LL int qu[200];
+
+int main(int argc, char const *argv[]) {
+	LL int Number;
+	cin >> Number;
+	while(Number--) {
+ 
+	}
+	return 0;
+}
