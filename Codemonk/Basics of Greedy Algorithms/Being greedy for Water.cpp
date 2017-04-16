9fdada92b56eb0a4da398ac91603f0443e1e10c5
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-04-16 17:48:34 +0530

    Practising greedy

---
+++ Codemonk/Basics of Greedy Algorithms/Being greedy for Water.cpp
@@ -0,0 +1,44 @@
+/*
+	Author : Amar Prakash Pandey
+	contact : http://amarpandey.me
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ll
+	#define ll long long
+#endif
+
+vector <ll int> vec;
+
+int main(int argc, char const *argv[]) {
+
+	long int T;
+	scanf("%ld", &T);
+
+	while(T--) {
+        ll int N, K, temp, total = 0;
+        scanf("%lld %lld", &N, &K);
+
+        for(ll int i=0; i < N; i++) {
+            scanf("%lld", &temp);
+            vec.push_back(temp);
+        }
+        
+        sort(vec.begin(), vec.end());
+        
+        temp = 0;
+        
+        for(ll int i=0; i < vec.size(); i++) {
+            total += vec.at(i);
+            if(total >= K)
+                break;
+            else
+                temp += 1;
+        }
+        printf("%lld\n", temp);
+        vec.clear();
+	}
+
+	return 0;
+}
