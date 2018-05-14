007f43bc626b880bc731b54706fb63beae11079e
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-05-14 12:26:07 +0530

    Create ToggleString.java

---
+++ Hackerearth/Basic Programming/ToggleString.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+class ToggleString {
+    
+    static class FastReader
+    {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(new
+                     InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    
+    public static void main(String args[] ) {
+        
+        FastReader fs = new FastReader();
+        String inputString = fs.next();
+        
+        for(int i = 0; i < inputString.length(); i++) {
+            int typeCast = (int) inputString.charAt(i);
+            
+            if(typeCast >= 65 && typeCast <= 90) {
+                typeCast += 32;
+            } else {
+                typeCast -= 32;
+            }
+            System.out.print((char)typeCast);
+        }
+
+    }
+}
